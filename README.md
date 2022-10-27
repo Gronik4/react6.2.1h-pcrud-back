03435c1930091cf5bb9d87f6fb3be65bc179a8e5
Gronik4 <golegni@yandex.ru>
2022-10-27 12:27:05 +0300

    Add url server

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 'Server for homework: react6.2.1h-pcrud'  
 блок React ДЗ №6.2.2(Жизненный цикл и работа с HTTP) задача: CRUD.backend   
-адрес: ()
+адрес: https://react621h-pcrud.herokuapp.com/
--- dataStart.js
+++ dataStart.js
@@ -8,4 +8,4 @@ export const startVdata = [
     text: 'N2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   }
 ]
-// url server: 
+// url server: https://react621h-pcrud.herokuapp.com/
